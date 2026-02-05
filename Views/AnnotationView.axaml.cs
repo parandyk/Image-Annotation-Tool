097c4dd11c730a91031c65e6b95f4684ce27347c
Michal Parandyk <1579870+parandyk@users.noreply.github.com>
2026-02-05 13:17:05 +0100

    major UI refinements; introduced common code for subviews, to allow reusage across tabs

---
+++ Views/AnnotationView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ImageAnnotationTool.Views;
+
+public partial class AnnotationView : UserControl
+{
+    public AnnotationView()
+    {
+        InitializeComponent();
+    }
+}
